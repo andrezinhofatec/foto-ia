5616caada8ade56948a401a65520942ea94855e3
andrezinhofatec <andrhenrique6488@gmail.com>
2021-03-14 14:45:17 -0300

    Adicionado novos commits

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ import carregando from '../src/images/Spinner-1s-64px.gif'
 function App(){
   const [pessoas, setPessoas] = useState([])
   const [carregando, Setcarregando] = useState(false)
+  const [idade, setIdade] = useState('')
+  const [etnia, setEtnia] = useState('')
 
 function ListaPessoas(){
   const listagemPessoas = pessoas.map((pessoa)=>
@@ -20,7 +22,10 @@ function ListaPessoas(){
 
   async function obterFoto(){
     Setcarregando(true)
-    let url = "https://api.generated.photos/api/v1/faces?api_key=7eLbLsAyGZDJO8fauA7ftA"
+    let chaveAPI = process.env.REACT_APP_APIKEY
+    const filtraEtnia = etnia.length > 0 ? `&ethnicity=${etnia}` : ''
+    const filtraIdade = idade.length > 0 ? `&age=${idade}` : ''
+    let url = `https://api.generated.photos/api/v1/faces?api_key=${chaveAPI}${filtraEtnia}${filtraIdade}&order_by=random`
     await fetch(url)
     .then(response => response.json())
     .then(data => {
@@ -40,12 +45,40 @@ function ListaPessoas(){
       {carregando &&
       <img src={carregando} title="Aguarde..." alt="Aguarde, dados sendo carregados" />
       }
-      <div className="fotos">
-      <ListaPessoas />
+      <div className="linha">
+        <ListaPessoas />
       </div>
-      <button type='button' onClick={obterFoto}>
-        Obter imagens
-      </button>
+
+      <div className="linha">
+        <label>Idade:</label>
+        <select onChange={event => setIdade(event.target.value)}>
+          <option value="">Todas</option>
+          <option value="infantil">infantil</option>
+          <option value="child">Criança</option>
+          <option value="young-adult">Jovem</option>
+          <option value="adult">Adulto</option>
+          <option value="elderly">Idoso</option>
+        </select>
+        <label>Etnia:</label>
+        <select onChange={e => setEtnia(e.target.value)}>
+          <option value="">Todas</option>
+          <option value="White">Branco</option>
+          <option value="Latino">Latino</option>
+          <option value="Asian">Asiático</option>
+          <option value="Black">Negro</option>
+        </select>
+      </div>
+
+      <div className="linha">
+        <button type='button' onClick={obterFoto}>
+          Obter imagens
+        </button>
+        {pessoas.length > 0 &&
+        <button type='button' onClick={() => setPessoas([])}>
+        Limpar imagens
+        </button>
+        }
+      </div>
     </div>
   )
 }
